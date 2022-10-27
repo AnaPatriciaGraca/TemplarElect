81d3dc5b39c86e69805fd832e7176ca06225edc8
AnaPatriciaGraca <78435927+AnaPatriciaGraca@users.noreply.github.com>
2022-10-27 01:52:42 +0000

    Completion of Register
    Correction of reading private key
    Creation of simetric key encrypted with public key (save to file and read from file)

--- src/register/Register.java
+++ src/register/Register.java
@@ -7,31 +7,45 @@ package register;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.Key;
+import java.security.KeyFactory;
 import java.security.KeyPair;
+import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.crypto.Cipher;
 import utils.SecurityUtils;
 
 /**
  *
  * @author AnaGraca
+ * 
+ * Classe Register permite:
+ *  - Gerar um par de chaves
+ *  - Guardar a chave privada no ficheiro USER.priv (onde USER corresponde ao nome do utilizador)
+ *  - Guardar a chave publica no ficheiro USER.pub (onde USER corresponde ao nome do utilizador)
+ *  - a chave privada do utilizador pode é encriptada por uma password escolhida pelo utilizador
+ *  - guardar uma chave simétrica e guardar essa chave encriptada com a chave publica (do ficheiro USER.pub)  
+ * 
  */
 public class Register {
     
     public Register(String user, String passwd){
         try {
+            //gerar par de chaves
             KeyPair kp = SecurityUtils.generateRSAKeyPair(2048);
+            //guardar chaves
             savePublicKey(user, kp);
             savePrivateKey(user, kp, passwd);
-            
+            saveSimetricKey_Pub(kp.getPublic(), user); 
         } catch (Exception ex) {
             Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
         }       
     }
-        
     
+    // ler chave publica do ficheiro USER.pub
     public static void savePublicKey(String user, KeyPair kp){
         try {
             SecurityUtils.saveKey(kp.getPublic(), user.concat(".pubkey"));
@@ -40,6 +54,7 @@ public class Register {
         }
     }
     
+    // guardar chave privada no ficheiro USER.priv, a chave privada fica encriptada com a password do user
     public static void savePrivateKey(String user, KeyPair kp, String passwd){
 
         try {
@@ -52,9 +67,27 @@ public class Register {
         }        
     }
     
-    public static PublicKey readPublicKey(String user){
-        String filename = user.concat(".pub");
+    // gerar chave simetrica, encriptar com a chave publica e guardar em USER.sim
+    public static void saveSimetricKey_Pub(PublicKey kPub, String user){
         try {
+            //gerar chave simetrica
+            Key k = SecurityUtils.generateAESKey(256);
+            System.out.println(k.toString());
+            //encriptar
+            byte[] secret = SecurityUtils.encrypt(k.getEncoded(), kPub);
+            // guardar num ficheiro
+            Files.write(Paths.get(user.concat(".sim")), secret);
+        } catch (Exception ex) {
+            Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+    }
+    
+    
+    //guardar chave publica no ficheiro desencriptado USER.pub 
+    public static PublicKey readPublicKey(String user){
+        String filename = user.concat(".pubkey");
+        try {
+            //Ler ficheiro da chave publica
             PublicKey key = SecurityUtils.loadPublicKey(filename);
             return key;
         } catch (IOException ex) {
@@ -62,29 +95,57 @@ public class Register {
             return null;
         }
     }
-    
-    public static void readPrivateKey(String user, String passwd){
-        String filename = user.concat(".pub");
+   
+    public static PrivateKey readPrivateKey(String user, String passwd){
+        String filename = user.concat(".privkey");
         try {
-            byte[] cipher = Files.readAllBytes(Paths.get(filename));
+            byte[] file = Files.readAllBytes(Paths.get(filename));
             try {
-                byte[] key = SecurityUtils.decrypt(cipher, passwd);
+                byte[] key = SecurityUtils.decrypt(file, passwd);
+                KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+                EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(key);
+                PrivateKey privateKey2 = keyFactory.generatePrivate(privateKeySpec);
+                return privateKey2;
             } catch (Exception ex) {
                 Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+                return null;
             }
-            
         } catch (IOException ex) {
             Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
         }
+    }
+    
+    // ler chave simetrica
+    public static byte[] readSimetricKey_Pub(String user, PrivateKey keyP){
+        String filename = user.concat(".sim");
+        System.out.println(keyP);
+        try {
+            //ler ficheiro
+            byte[] secret = Files.readAllBytes(Paths.get(user.concat(".sim")));
+            //desencriptar os dados
+            byte []  plain = SecurityUtils.decrypt(secret, keyP);
+            //retornar chave simetrica
+            return plain;
+        } catch (Exception ex) {
+            Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        
     }
             
             
-//    public static void main(String[] args) {
-//        String user = "Ana";
-//        String passwd = "12qweasd!#";     
-//        
-//        Register r = new Register(user, passwd);
-//    }
+    public static void main(String[] args) {
+        String user = "Ana";
+        String passwd = "12qweasd!#";     
+        
+        Register r = new Register(user, passwd);
+        System.out.println(r.readPrivateKey(user, passwd));
+        
+        System.out.println(r.readSimetricKey_Pub(user, r.readPrivateKey(user, passwd)));
+        
+        
+    }
     
     
 }
